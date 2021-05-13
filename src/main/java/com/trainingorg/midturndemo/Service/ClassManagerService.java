d61b5134b42ff7c965a3ef13cc4f63594a629708
chococc <liukaimeng@163.com>
2021-05-13 16:47:42 +0800

    nothing

--- src/main/java/com/trainingorg/midturndemo/Service/ClassManagerService.java
+++ src/main/java/com/trainingorg/midturndemo/Service/ClassManagerService.java
@@ -1,24 +1,20 @@
 package com.trainingorg.midturndemo.Service;
 
 import com.trainingorg.midturndemo.Util.MysqlActuator;
-import com.trainingorg.midturndemo.Util.TimeStamp;
 import com.trainingorg.midturndemo.bean.Entity.ClassEntity;
 import com.trainingorg.midturndemo.bean.HttpRequest;
 import org.springframework.stereotype.Service;
-
-import java.sql.Timestamp;
 
 @Service
 public class ClassManagerService {
 
     MysqlActuator mysqlActuator=new MysqlActuator();
     HttpRequest httpRequest=new HttpRequest();
-    TimeStamp timeStamp=new TimeStamp();
 
     public HttpRequest addClassManagerService(String teacherId,String courseID,String startTime, String stopTime){
         ClassEntity classEntity=new ClassEntity(teacherId,courseID,startTime,stopTime);
         try{
-            mysqlActuator.update("INSERT INTO ClassList(teacherid,courseId,starttime,endtime) Values('"+classEntity.getTeacherId()+"','"+classEntity.getCourseId()+"','"+classEntity.getStartTime()+"','"+classEntity.getStopTime()+"')");
+            mysqlActuator.update("INSERT INTO ClassList(teacherId,courseId,startTime,endTime) Values('"+classEntity.getTeacherId()+"','"+classEntity.getCourseId()+"','"+classEntity.getStartTime()+"','"+classEntity.getStopTime()+"')");
             httpRequest.setRequestCode(200);
             httpRequest.setRequestMessage("班级创建成功");
         }catch (Exception e){
