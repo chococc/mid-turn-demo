d6cf2621a296628be539131b95647db4aa56c6fa
chococc <liukaimeng@163.com>
2021-05-14 15:34:53 +0800

    nothing

--- src/main/java/com/trainingorg/midturndemo/Service/ClassManagerService.java
+++ src/main/java/com/trainingorg/midturndemo/Service/ClassManagerService.java
@@ -24,4 +24,31 @@ public class ClassManagerService {
         }
         return httpRequest;
     }
+
+    public HttpRequest deleteClassService(String classID){
+        try{
+            mysqlActuator.update("DELETE FROM classList where classID="+classID);
+            httpRequest.setRequestCode(200);
+            httpRequest.setRequestMessage("班级信息删除成功");
+        }catch (Exception e){
+            e.printStackTrace();
+            httpRequest.setRequestCode(402);
+            httpRequest.setRequestMessage("课程信息删除失败");
+        }
+        return httpRequest;
+    }
+
+    public HttpRequest updateClassService(String teacherId,String courseID,String startTime, String stopTime){
+        ClassEntity classEntity=new ClassEntity(teacherId,courseID,startTime,stopTime);
+        try{
+            mysqlActuator.update("UPDATE classList SET teacherId='"+classEntity.getTeacherName()+"',courseId='"+classEntity.getCourseId()+"',startTime='"+classEntity.getStartTime()+"',stopTime='"+classEntity.getStopTime()+"',courseName='"+classEntity.getCourseName()+"',teacherName='"+classEntity.getTeacherName()+"'");
+            httpRequest.setRequestCode(200);
+            httpRequest.setRequestMessage("班级信息修改成功");
+        }catch (Exception e){
+            e.printStackTrace();
+            httpRequest.setRequestCode(403);
+            httpRequest.setRequestMessage("课程信息修改失败");
+        }
+        return httpRequest;
+    }
 }
--- src/main/java/com/trainingorg/midturndemo/Service/UserService.java
+++ src/main/java/com/trainingorg/midturndemo/Service/UserService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
 
-
 @Service
 
 public class UserService {
