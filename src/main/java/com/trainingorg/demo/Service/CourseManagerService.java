ca880d8cb10fda2b8ba59aa9382c1fc91505494f
coco7 <liukaimeng@163.com>
2021-06-14 19:47:54 +0800

    nothing

--- src/main/java/com/trainingorg/demo/Service/CourseManagerService.java
+++ src/main/java/com/trainingorg/demo/Service/CourseManagerService.java
@@ -1,5 +1,7 @@
 package com.trainingorg.demo.Service;
 
+import com.trainingorg.demo.Util.NoToken;
+import com.trainingorg.demo.Util.Token;
 import com.trainingorg.demo.bean.HttpRequest;
 import com.trainingorg.demo.dao.CourseManageDao;
 import org.springframework.stereotype.Service;
@@ -13,9 +15,15 @@ public class CourseManagerService {
 
     public HttpRequest addCourseService(String courseName){
         try {
+            new Token().IdentityCheck("orgManager");
             courseManageDao.addCourse(courseName);
             httpRequest.setCode(200);
             httpRequest.setRequestMessage("课程添加成功"+courseName);
+        } catch (NoToken n) {
+            n.printStackTrace();
+            httpRequest.setCode(100);
+            httpRequest.setRequestMessage("用户未登入,或使用了非管理员账号.");
+            return httpRequest;
         }catch (Exception e){
             e.printStackTrace();
             httpRequest.setCode(301);
@@ -27,9 +35,15 @@ public class CourseManagerService {
 
     public HttpRequest deleteCourseService(String courseName){
         try {
+            new Token().IdentityCheck("orgManager");
             courseManageDao.deleteCourse(courseName);
             httpRequest.setCode(200);
             httpRequest.setRequestMessage("课程删除成功："+courseName);
+        }catch (NoToken n) {
+            n.printStackTrace();
+            httpRequest.setCode(100);
+            httpRequest.setRequestMessage("用户未登入,或使用了非管理员账号.");
+            return httpRequest;
         }catch (Exception e){
             e.printStackTrace();
             httpRequest.setCode(302);
@@ -40,9 +54,15 @@ public class CourseManagerService {
 
     public HttpRequest editCourseService(String courseName,float cost,int state){
         try{
+            new Token().IdentityCheck("orgManager");
             courseManageDao.editCourse(courseName,cost,state);
             httpRequest.setCode(200);
             httpRequest.setRequestMessage("课程修改成功："+courseName);
+        }catch (NoToken n) {
+            n.printStackTrace();
+            httpRequest.setCode(100);
+            httpRequest.setRequestMessage("用户未登入,或使用了非管理员账号.");
+            return httpRequest;
         }catch (Exception e){
             e.printStackTrace();
             httpRequest.setCode(303);
@@ -53,9 +73,15 @@ public class CourseManagerService {
 
     public HttpRequest selectAllService(){
         try {
+            new Token().IdentityCheck("orgManager");
             httpRequest.setData(courseManageDao.selectAll());
             httpRequest.setCode(200);
             httpRequest.setRequestMessage("用户信息拉取成功");
+        }catch (NoToken n) {
+            n.printStackTrace();
+            httpRequest.setCode(100);
+            httpRequest.setRequestMessage("用户未登入,或使用了非管理员账号.");
+            return httpRequest;
         }catch (Exception e){
             httpRequest.setCode(104);
             httpRequest.setRequestMessage("拉取用户信息失败");
@@ -66,9 +92,15 @@ public class CourseManagerService {
 
     public HttpRequest selectByID(String courseName){
         try{
+            new Token().IdentityCheck("orgManager");
             httpRequest.setCode(200);
             httpRequest.setRequestMessage("数据拉取成功");
             httpRequest.setData(courseManageDao.selectByName(courseName));
+        }catch (NoToken n) {
+            n.printStackTrace();
+            httpRequest.setCode(100);
+            httpRequest.setRequestMessage("用户未登入,或使用了非管理员账号.");
+            return httpRequest;
         }catch (Exception e){
             e.printStackTrace();
             httpRequest.setCode(306);
